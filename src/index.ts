062c04da3120f5d0cfd176701f92732654dd64e5
mojahige <mojahige@gmail.com>
2022-09-22 08:22:44 +0900

    change file name

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import * as Turbo from "@hotwired/turbo";
 import { Application } from "@hotwired/stimulus";
-import DropdownMenuController from "./controllers/dropdown_menu_controller.js.js";
+import DropdownMenuController from "./controllers/dropdown_menu_controller.js";
 
 const register = () => {
   const app = Application.start();
